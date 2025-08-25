3814f2c3ac299d13f8c15c543d55e92a4ce20676
shamooooot <shamooooot@hotmail.com>
2025-08-25 21:50:27 +0300

    First commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,25 @@
+package org.example;
+
+public class Calculator {
+
+    public enum Operator {
+        ADD, SUBTRACT, MULTIPLY, DIVIDE
+    }
+
+    public double calculate(double a, double b, Operator op) {
+        if (op == Operator.ADD) {
+            return a + b;
+        } else if (op == Operator.SUBTRACT) {
+            return a - b;
+        } else if (op == Operator.MULTIPLY) {
+            return a * b;
+        } else if (op == Operator.DIVIDE) {
+            if (b == 0) {
+                throw new ArithmeticException("Cannot divide by zero");
+            }
+            return a / b;
+        } else {
+            throw new IllegalArgumentException("Unknown operator");
+        }
+    }
+}
